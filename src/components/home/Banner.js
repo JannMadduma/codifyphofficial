0ca2023127c4766aa6810c43b4a8135926b34e50
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-09-16 22:09:51 +0800

    linked app,home,banner and added photos

---
+++ src/components/home/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Banner = ({}) => {
+  return (
+    <div className="row">
+      <div className="col"></div>
+      <div className="col"></div>
+    </div>
+  );
+};
+
+export default Banner;
