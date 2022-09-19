e6b254c2b3edc682fd7d42a5305cce65ae21eaf1
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-09-19 13:42:10 +0800

    gallery

---
+++ src/components/home/Gallery.js
@@ -0,0 +1,215 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Grid from "@mui/material/Unstable_Grid2";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { IconButton } from "@mui/material";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
+    title: "Bed",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
+    title: "Books",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
+    title: "Sink",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
+    title: "Kitchen",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
+    title: "Blinds",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
+    title: "Chairs",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
+    title: "Laptop",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
+    title: "Doors",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
+    title: "Storage",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+    title: "Candle",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+    title: "Coffee table",
+  },
+];
+
+const theme = createTheme();
+
+export default function Gallery() {
+  const [step, setStep] = React.useState(0);
+  const handleBack = () => {
+    if (step > 0) {
+      setStep(step - 1);
+    } else {
+      setStep(2);
+    }
+  };
+
+  const handleNext = () => {
+    if (step < 2) {
+      setStep(step + 1);
+    } else {
+      setStep(0);
+    }
+  };
+
+  React.useEffect(() => {
+    const container = document.querySelector(".scrollGallery");
+    if (step === 0) {
+      container.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    } else if (step === 1) {
+      container.scrollTo({
+        top: 0,
+        left: (container.scrollWidth - container.clientWidth) / 2,
+        behavior: "smooth",
+      });
+    } else {
+      container.scrollTo({
+        top: 0,
+        left: container.scrollWidth - container.clientWidth,
+        behavior: "smooth",
+      });
+    }
+  }, [step]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <main>
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container>
+            <Typography
+              component="h1"
+              variant="h4"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Explore homes
+            </Typography>
+            <Typography align="center" color="text.secondary" paragraph>
+              Take a deep dive and browse homes for sale, original neighborhood
+              <br />
+              photos, resident reviews and local insights to find what is right
+              for you.
+            </Typography>
+          </Container>
+        </Box>
+      </main>
+      <Container
+        sx={{
+          margin: "0px",
+          maxWidth: { xs: "100%" },
+          padding: { xs: "0px" },
+        }}
+      >
+        <div>
+          <Grid
+            container
+            sx={{
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            <Grid xs={12}>
+              <Box
+                className="scrollGallery"
+                sx={{
+                  width: "100%",
+                  overflowX: "hidden",
+                  overflowY: "hidden",
+                }}
+              >
+                <Box
+                  sx={{
+                    width: "200%",
+                  }}
+                >
+                  <ImageList variant="masonry" cols={10} gap={8}>
+                    {itemData.map((item) => (
+                      <ImageListItem key={item.img}>
+                        <img
+                          src={`${item.img}?w=248&fit=crop&auto=format`}
+                          srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                          alt={item.title}
+                          loading="lazy"
+                        />
+                      </ImageListItem>
+                    ))}
+                  </ImageList>
+                </Box>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  width: "100%",
+                  justifyContent: "space-between",
+                  position: "absolute",
+                  top: "calc(50% - 30px)",
+                  padding: "0px 20px",
+                }}
+              >
+                <IconButton
+                  aria-label="delete"
+                  onClick={handleBack}
+                  sx={{
+                    background: "white",
+                  }}
+                >
+                  <ArrowBackIosNewIcon />
+                </IconButton>
+                <IconButton
+                  aria-label="delete"
+                  onClick={handleNext}
+                  sx={{
+                    background: "white",
+                  }}
+                >
+                  <ArrowForwardIosIcon />
+                </IconButton>
+              </Box>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </ThemeProvider>
+  );
+}
