ed21faf5eb0a8930198cffd8ea6217e9f1f99979
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-09-19 14:13:10 +0800

    footerphoto

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import Gallery from "./Gallery";
+import Stats from "./Stats";
 
 const Home = ({}) => {
   return (
     <div>
       <Banner />
       <Gallery />
+      <Stats />
     </div>
   );
 };
