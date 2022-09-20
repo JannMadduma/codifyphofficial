567f766d359258ac92821684a9c3132151c23b81
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-09-20 14:46:58 +0800

    ManageProperties

--- src/components/admin/ManageProperties.js
+++ src/components/admin/ManageProperties.js
@@ -1,17 +1,60 @@
 import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import { Typography } from "@mui/material";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Link from "@mui/material/Link";
+import { mainListItems } from "./listItems";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProperties } from "../../service/propertyService";
 import { setProperties } from "../../actions/propertiesActions";
+import { Table } from "react-bootstrap";
+import { TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 
-export default function ManageProperties() {
+const drawerWidth = 240;
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
   const dispatch = useDispatch();
   const properties = useSelector((state) => state.properties);
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   React.useEffect(() => {
     getAllProperties().then((res) => {
@@ -20,46 +63,88 @@ export default function ManageProperties() {
   }, []);
 
   return (
-    <div>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Property Lisiting</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Property Name</TableCell>
-            <TableCell>Location</TableCell>
-            <TableCell>Developer</TableCell>
-            <TableCell>Project Type</TableCell>
-            <TableCell>Monthly Payment</TableCell>
-            <TableCell>TCP</TableCell>
-            <TableCell>BedRooms</TableCell>
-            <TableCell>BathRooms</TableCell>
-            <TableCell>LotArea</TableCell>
-            <TableCell>FloorArea</TableCell>
-            <TableCell align="right">Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {properties.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.propertyName}</TableCell>
-              <TableCell>{row.location}</TableCell>
-              <TableCell>{row.developer}</TableCell>
-              <TableCell>{row.projectType}</TableCell>
-              <TableCell>{row.monthly}</TableCell>
-              <TableCell>{row.tcp}</TableCell>
-              <TableCell>{row.bedRooms}</TableCell>
-              <TableCell>{row.bathRooms}</TableCell>
-              <TableCell>{row.lotArea}</TableCell>
-              <TableCell>{row.floorArea}</TableCell>
-              <TableCell align="right"></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          ></Toolbar>
+
+          <List component="nav">{mainListItems}</List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                  <Table size="small">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Properties</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Property Name</TableCell>
+                        <TableCell>Location</TableCell>
+                        <TableCell>Developer</TableCell>
+                        <TableCell>Project Type</TableCell>
+                        <TableCell>Monthly Payment</TableCell>
+                        <TableCell>TCP</TableCell>
+                        <TableCell>BedRooms</TableCell>
+                        <TableCell>BathRooms</TableCell>
+                        <TableCell>LotArea</TableCell>
+                        <TableCell>FloorArea</TableCell>
+                        <TableCell align="right">Actions</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {properties.map((row) => (
+                        <TableRow key={row.id}>
+                          <TableCell>{row.id}</TableCell>
+                          <TableCell>{row.propertyName}</TableCell>
+                          <TableCell>{row.location}</TableCell>
+                          <TableCell>{row.developer}</TableCell>
+                          <TableCell>{row.projectType}</TableCell>
+                          <TableCell>{row.monthly}</TableCell>
+                          <TableCell>{row.tcp}</TableCell>
+                          <TableCell>{row.bedRooms}</TableCell>
+                          <TableCell>{row.bathRooms}</TableCell>
+                          <TableCell>{row.lotArea}</TableCell>
+                          <TableCell>{row.floorArea}</TableCell>
+                          <TableCell align="right"></TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
+}
+
+export default function ManageProperties() {
+  return <DashboardContent />;
 }
---
+++ src/components/admin/listItems.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import GroupIcon from "@mui/icons-material/Group";
+import NightShelterIcon from "@mui/icons-material/NightShelter";
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <NightShelterIcon />
+      </ListItemIcon>
+      <ListItemText primary="Properties" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <GroupIcon />
+      </ListItemIcon>
+      <ListItemText primary="Users" />
+    </ListItemButton>
+  </React.Fragment>
+);
