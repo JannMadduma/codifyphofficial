59bd03f195aebbae5834145348d5b13c6f3bb1a3
TonSalingua <j.anton.salingua@gmail.com>
2022-09-20 15:28:07 +0800

    added users to dashboard

--- src/ComponentRoute.js
+++ src/ComponentRoute.js
@@ -3,12 +3,16 @@ import Home from "./components/home/Home";
 import Listing from "./components/listing/list/Listing";
 
 import { Route, Routes } from "react-router-dom";
+import ManageProperties from "./components/admin/ManageProperties";
+import Manageusers from "./components/admin/ManageUser";
 const ComponentRoute = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/listing" element={<Listing />} />
+      <Route path="/manageproperty" element={<ManageProperties />} />
+      <Route path="/manageusers" element={<Manageusers />} />
     </Routes>
   );
 };
--- src/components/admin/ManageProperties.js
+++ src/components/admin/ManageProperties.js
@@ -3,20 +3,18 @@ import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import MuiDrawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
-import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Link from "@mui/material/Link";
 import { mainListItems } from "./listItems";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProperties } from "../../service/propertyService";
 import { setProperties } from "../../actions/propertiesActions";
 import { Table } from "react-bootstrap";
 import { TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import ResponsiveAppBar from "../common/Navbar";
 
 const drawerWidth = 240;
 
@@ -64,6 +62,7 @@ function DashboardContent() {
 
   return (
     <ThemeProvider theme={mdTheme}>
+    <ResponsiveAppBar/>
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
 
