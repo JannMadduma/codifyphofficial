5f374bc9018d2da846324927fbdf083ee98cd2e4
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-09-21 22:34:43 +0800

    listing card

--- src/components/home/TopListing.js
+++ src/components/home/TopListing.js
@@ -2,22 +2,14 @@ import { Box, Grid, Paper } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import Container from "@mui/material/Container";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import HotelIcon from "@mui/icons-material/Hotel";
-import BathtubIcon from "@mui/icons-material/Bathtub";
-import SquareFootIcon from "@mui/icons-material/SquareFoot";
 import { useEffect } from "react";
 import { getAllProperties } from "../../service/propertyService";
 import { setProperties } from "../../actions/propertiesActions";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ListingCard from "../common/ListingCard";
 
 export default function TopListing() {
   const dispatch = useDispatch();
@@ -109,95 +101,7 @@ export default function TopListing() {
           <Grid container columns={9} sx={{ width: "200%" }}>
             {properties.slice(0, 9).map((property) => (
               <Grid item xs={1}>
-                <Card sx={{ position: "relative" }} variant="outlined">
-                  <Paper
-                    variant="outlined"
-                    sx={{
-                      position: "absolute",
-                      top: 8,
-                      left: 8,
-                      padding: "2px 5px",
-                    }}
-                  ></Paper>
-                  <CardMedia
-                    component="img"
-                    height="194"
-                    image={property.img[0]}
-                  />
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      sx={{ textAlign: "left" }}
-                    >
-                      ₱{" "}
-                      {property.tcp
-                        .toString()
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      sx={{
-                        textAlign: "left",
-                        height: "45px",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        display: "-webkit-box",
-                        "-webkit-line-clamp": "2",
-                        "-webkit-box-orient": "vertical",
-                      }}
-                    >
-                      {property.propertyName}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      sx={{ textAlign: "left", padding: "10px 0" }}
-                    >
-                      {property.bedRooms}
-                      <IconButton aria-label="delete" size="small">
-                        <HotelIcon fontSize="inherit" />
-                      </IconButton>
-                      {property.bathRooms}
-                      <IconButton aria-label="delete" size="small">
-                        <BathtubIcon fontSize="inherit" />
-                      </IconButton>
-                      {property.lotArea}
-                      <IconButton aria-label="delete" size="small">
-                        <SquareFootIcon fontSize="inherit" />
-                      </IconButton>
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      sx={{
-                        textAlign: "left",
-                        height: "45px",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        display: "-webkit-box",
-                        "-webkit-line-clamp": "2",
-                        "-webkit-box-orient": "vertical",
-                      }}
-                    >
-                      {property.location}
-                    </Typography>
-
-                    <hr />
-                    {loggedIn?.id && (
-                      <CardActions disableSpacing>
-                        <IconButton aria-label="add to favorites" size="small">
-                          <FavoriteIcon fontSize="inherit" />
-                        </IconButton>
-                        <IconButton aria-label="share" size="small">
-                          <ShareIcon fontSize="inherit" />
-                        </IconButton>
-                      </CardActions>
-                    )}
-                  </CardContent>
-                </Card>
+                <ListingCard property={property} loggedIn={loggedIn} />
               </Grid>
             ))}
           </Grid>
--- src/components/listing/list/List.js
+++ src/components/listing/list/List.js
@@ -12,20 +12,11 @@ import {
 import { useSelector, useDispatch } from "react-redux";
 import Container from "@mui/material/Container";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import HotelIcon from "@mui/icons-material/Hotel";
-import BathtubIcon from "@mui/icons-material/Bathtub";
-import SquareFootIcon from "@mui/icons-material/SquareFoot";
 import { useEffect } from "react";
 import { getAllProperties } from "../../../service/propertyService";
 import { setProperties } from "../../../actions/propertiesActions";
+import ListingCard from "../../common/ListingCard";
 
 export default function List() {
   const dispatch = useDispatch();
@@ -139,95 +130,7 @@ export default function List() {
         <Grid container columns={12}>
           {properties.slice(0, 100).map((property) => (
             <Grid item sm={6} md={4} lg={3} xs={12}>
-              <Card sx={{ position: "relative" }} variant="outlined">
-                <Paper
-                  variant="outlined"
-                  sx={{
-                    position: "absolute",
-                    top: 8,
-                    left: 8,
-                    padding: "2px 5px",
-                  }}
-                ></Paper>
-                <CardMedia
-                  component="img"
-                  height="194"
-                  image={property.img[0]}
-                />
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                    sx={{ textAlign: "left" }}
-                  >
-                    ₱{" "}
-                    {property.tcp
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    sx={{
-                      textAlign: "left",
-                      height: "45px",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      display: "-webkit-box",
-                      "-webkit-line-clamp": "2",
-                      "-webkit-box-orient": "vertical",
-                    }}
-                  >
-                    {property.propertyName}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    sx={{ textAlign: "left", padding: "10px 0" }}
-                  >
-                    {property.bedRooms}
-                    <IconButton aria-label="delete" size="small">
-                      <HotelIcon fontSize="inherit" />
-                    </IconButton>
-                    {property.bathRooms}
-                    <IconButton aria-label="delete" size="small">
-                      <BathtubIcon fontSize="inherit" />
-                    </IconButton>
-                    {property.lotArea}
-                    <IconButton aria-label="delete" size="small">
-                      <SquareFootIcon fontSize="inherit" />
-                    </IconButton>
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    sx={{
-                      textAlign: "left",
-                      height: "45px",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      display: "-webkit-box",
-                      "-webkit-line-clamp": "2",
-                      "-webkit-box-orient": "vertical",
-                    }}
-                  >
-                    {property.location}
-                  </Typography>
-
-                  <hr />
-                  {loggedIn?.id && (
-                    <CardActions disableSpacing>
-                      <IconButton aria-label="add to favorites" size="small">
-                        <FavoriteIcon fontSize="inherit" />
-                      </IconButton>
-                      <IconButton aria-label="share" size="small">
-                        <ShareIcon fontSize="inherit" />
-                      </IconButton>
-                    </CardActions>
-                  )}
-                </CardContent>
-              </Card>
+              <ListingCard property={property} loggedIn={loggedIn} />
             </Grid>
           ))}
         </Grid>
