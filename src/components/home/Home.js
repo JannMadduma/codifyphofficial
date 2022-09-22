9c5d770e9d421a6e8847b5ba28dd7e851af2850b
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2022-09-22 21:03:13 +0800

    call to action and subscribe

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,15 +5,28 @@ import Banner from "./Banner";
 import Gallery from "./Gallery";
 import Stats from "./Stats";
 import TopListing from "./TopListing";
+import CallToAction from "../common/CallToAction";
+import { Box } from "@mui/system";
+import Subscribe from "../common/subscribe";
 
 const Home = ({}) => {
   return (
     <div>
       <ResponsiveAppBar />
       <Banner />
+      <Box sx={{ padding: "50px" }}>
+        <CallToAction />
+      </Box>
       <Gallery />
       <Stats />
       <TopListing />
+      <Box sx={{ padding: "50px 0" }}>
+        <Subscribe />
+      </Box>
+      <Box sx={{ padding: "50px" }}>
+        <CallToAction />
+      </Box>
+
       <StickyFooter />
     </div>
   );
--- src/components/listing/list/Listing.js
+++ src/components/listing/list/Listing.js
@@ -1,13 +1,22 @@
+import { Box } from "@mui/system";
 import React from "react";
+import CallToAction from "../../common/CallToAction";
 import StickyFooter from "../../common/Footer";
 import ResponsiveAppBar from "../../common/Navbar";
+import Subscribe from "../../common/subscribe";
 import List from "./List";
 
 const Listing = ({}) => {
   return (
     <React.Fragment>
       <ResponsiveAppBar />
-      <List />
+      <Subscribe />
+      <Box sx={{ paddingTop: "10px" }}>
+        <List />
+      </Box>
+      <Box sx={{ padding: "50px" }}>
+        <CallToAction />
+      </Box>
       <StickyFooter />
     </React.Fragment>
   );
