93f499eca4bd613ffe52da7ecfb1679ed9b76d5a
Jann Fraulien Madduma <madduma.jannfraulien@gmail.com>
2026-02-24 17:21:11 +0800

    use hashrouter

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import { Provider } from "react-redux";
 import ComponentRoute from "./ComponentRoute";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import configureStore from "./store/configureStore";
 const store = configureStore({});
 
 function App() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <ComponentRoute />
-      </BrowserRouter>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <HashRouter>
+                <ComponentRoute />
+            </HashRouter>
+        </Provider>
+    );
 }
 
 export default App;
